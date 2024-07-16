60ae1d4587c67fba12920dac662d53e5c8c46625
Tamás Morcsányi <mtomi8@gmail.com>
2024-07-16 10:13:16 +0200

    add fetch error handling

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 const APOD_URL = "https://api.nasa.gov/planetary/apod?api_key=q5CY83py5gbzhu1pMa82D3zvNQOy6cwlEwXBzHMH"
 
+async function fetchApod(url){
+    const response = await fetch(url);
+    if(response.ok){
+       return await response.json();
+    } else {
+        throw new Error("error message");
+    }
+}
+
+async function main(){
+    try {
+        const data = await fetchApod(APOD_URL);
+        console.log(data);
+    } catch (error) {
+        console.log(error);   
+    }
+
+}
+
+main();
