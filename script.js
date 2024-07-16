361cfd0bb250d9f920596eb897a4c0006a1ff9b4
Tamás Morcsányi <mtomi8@gmail.com>
2024-07-16 10:27:14 +0200

    add III option

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
 const APOD_URL = "https://api.nasa.gov/planetary/apod?api_key=q5CY83py5gbzhu1pMa82D3zvNQOy6cwlEwXBzHMH"
 
-async function fetchApod(url){
-    const response = await fetch(url);
-    if(response.ok){
-       return await response.json();
-    } else {
-        throw new Error("error message");
-    }
+// I, II
+// async function fetchApod(url){
+//     const response = await fetch(url);
+//     if(response.ok){
+//        return await response.json();
+//     } else {
+//         throw new Error("error message");
+//     }
+// }
+
+// III.
+function fetchApod(url){
+    return fetch(url).then((response) => {
+        if(response.ok){
+            return response.json();
+        } else {
+            throw new Error("error message");
+        }
+    })
 }
 
 async function main(){
-    try {
-        const data = await fetchApod(APOD_URL);
-        console.log(data);
-    } catch (error) {
-        console.log(error);   
-    }
+    // I.
+    // try {
+    //     const data = await fetchApod(APOD_URL);
+    //     console.log(data);
+    // } catch (error) {
+    //     console.log(error);   
+    // }
+
+    // II.
+    // fetchApod(APOD_URL)
+    // .then((data) => {
+    //     console.log(data);
+    // })
+    // .catch((error) => {
+    //     console.log(error);
+    // })
+
+    // III.
+    fetchApod(APOD_URL).then(data => console.log(data)).catch(error => console.log(error));
 
 }
 
